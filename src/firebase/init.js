9ee28f9d47ee1189e2076288af872ae21bf98b67
nour-jaouadii <56494614+nour-jaouadii@users.noreply.github.com>
2020-04-19 20:59:48 +0100

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBljVU0vAdR_fgiQRSDUmwSOZa7yHAC3cg",
-  authDomain: "ninja-smoothies-67ec1.firebaseapp.com",
-  databaseURL: "https://ninja-smoothies-67ec1.firebaseio.com",
-  projectId: "ninja-smoothies-67ec1",
-  storageBucket: "ninja-smoothies-67ec1.appspot.com",
-  messagingSenderId: "408619891736",
-  appId: "1:408619891736:web:e3795501dcc1ec06d6d76d",
-  measurementId: "G-PFHN7L9HPL",
+your config application  
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
